ee92fbb224670c8075a4b144b1f9c3fe6fad796b
aderocher <am.derocher@gmail.com>
2021-09-13 17:01:48 -0700

    router for spots works!

--- seabnb-project/frontend/src/components/SpotBrowser/index.js
+++ seabnb-project/frontend/src/components/SpotBrowser/index.js
@@ -1,10 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { Route } from 'react-dom';
+import React, { useEffect } from 'react';
+// import { Route } from 'react-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getSpots } from "../../store/spots";
 import './SpotBrowser.css';
 
 function SpotBrowser(){
+
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getSpots());
+  }, [ dispatch ]);
+
+  const spots = useSelector(state => {
+    return state;
+  });
+  console.log(spots,'<==============================')
+
   return(
-    <h2>I'm Spot Browser</h2>
+    <div>
+      <h2>I'm Spot Browser</h2>
+    </div>
   )
 }
 

ee4aa9114ef36923d5c7f27d7c848f3be604844d
aderocher <am.derocher@gmail.com>
2021-09-14 18:40:47 -0700

    got the date inputs and checkers working

---
+++ seabnb-project/frontend/src/components/ReserveSpotForm/dateUtils.js
@@ -0,0 +1,33 @@
+import { areIntervalsOverlapping } from 'date-fns'
+import { format } from 'date-fns'
+import { addDays } from 'date-fns'
+
+
+
+export const today = () => {
+  const day = format(new Date(),'yyyy-MM-dd');
+  return day;
+}
+export const tomorrow = () => {
+  let day = new Date();
+  let morrow = addDays(day, 1);
+  let tomorrow = format(morrow,'yyyy-MM-dd')
+  return tomorrow
+}
+
+export const datesOverlap = (booking, newBooking) => {
+  const overlap = areIntervalsOverlapping(
+    { start:booking.checkIn , end: booking.checkOut },
+    { start: newBooking.checkIn, end: newBooking.checkOut}
+  )
+  console.log(overlap);
+  return overlap;
+}
+
+
+const bookingValidator = (spotId) => {
+  // all bookings = all of the bookings with this spotId
+  // for each booking in bookings: datesOverlap === false? book
+                                            // : return error
+}
+
--- seabnb-project/frontend/src/components/ReserveSpotForm/index.js
+++ seabnb-project/frontend/src/components/ReserveSpotForm/index.js
@@ -1,45 +1,78 @@
 import React from "react";
 import { useHistory } from "react-router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { today, tomorrow, datesOverlap } from './dateUtils';
 
 const ReserveSpotForm = () => {
-
-  const [ startDate, setStartDate ] = useState('')
-  const [ endDate, setEndDate ] = useState('')
   const history = useHistory();
+  const dispatch = useDispatch();
+  const { spotId } = useParams();
+
+  let day = today();
+  let morrow = tomorrow();
+  const [ startDate, setStartDate ] = useState(day);
+  const [ endDate, setEndDate ] = useState(morrow);
+  const [ errors, setErrors ] = useState([]);
+
+  // useEffect(()=>{
+  //   dispatch(getOneSpot(spotId));
+  // }, [ dispatch, spotId ]);
+    
+  const spot = useSelector(state => state.spots.spot );
+  const user = useSelector(state => state.session.user );
+
+  useEffect((today)=>{
+    setStartDate(today);
+  },[ startDate ])
+
+  useEffect(()=> {
+    let newErrors = [];
+    if (startDate < today ) newErrors.push('Reservations can not be made in the past!');
+    if (!startDate) newErrors.push('Reservations can not be made in the past!');
+    if (!endDate) newErrors.push('Reservations can not be made in the past!');
+    console.log('Reservations can not be made in the past!');
+
+    setErrors(newErrors);
+  }, [ startDate, endDate ])
 
   const submitReservation = () => {
     
-    // const {  }
-    
+    console.log(startDate, '<===>',endDate)
+    const body ={
+      guest:user,
+      spot:spot,
+      checkIn:startDate,
+      checkOut:endDate
+    }
+    console.log('form: ', body)
     history.push('/');
+  }
+
+  const logDates = (e) =>{
+    e.preventDefault();
+    console.log("start: ", startDate)
+    console.log("end: ", endDate)
+    // console.log("fullDate: ", fullDate)
+    // console.log("currDate: ", currDate)
+    console.log("day: ", day)
+    console.log("morrow: ", morrow)
   }
 
   return(
     <div>
       <form className="" onSubmit={submitReservation}>
-        <input type="date" name="checkIn"></input>
-        <input type="date" name="checkOut"></input>          
-        <button>Reserve</button>
+        <input type="date" name="checkIn" min={today} value={startDate} onChange={e=> setStartDate(e.target.value)}></input>
+        <input type="date" name="checkOut" min={startDate} value={endDate} onChange={e=> setEndDate(e.target.value)}></input>          
+        <button disabled={errors.length > 0}>Reserve</button>
+        <button onClick={e => logDates(e)}>Console Log Dates</button>
       </form>
     </div>
   )
 }
 
 export default ReserveSpotForm;
-
-
-
-console.log(2000-01-02 > 2000-1-1) // false
-console.log(2000-01-02 === 2000-01-02) // true
-console.log(2001-01-02 > 2000-9-9) // true
-
-console.log(2000-9-9 > 1999-1-1) // false
-console.log(2000-01-02 > 2000-)
-
-
-const dateControl1 = ());
-const dateControl2 = 1999-1-1;
 
 
 

18e767485eac465b4fbeb72a08180d815959fb9f
aderocher <am.derocher@gmail.com>
2021-09-15 18:22:32 -0700

    cards are in a good place

--- seabnb-project/backend/routes/api/bookings.js
+++ seabnb-project/backend/routes/api/bookings.js
@@ -12,17 +12,6 @@ router.get('/', asyncHandler(async (req, res) => {
   return res.json(allBookings);
 }));
 
-// // ====== get all the bookings for current spot =======
-// router.get('/:id', asyncHandler(async (req, res) => {
-//   const spotId = parseInt(req.params, 10);
-//   const allSpotBookings = await Booking.findByPk({
-//     where: {
-//       spot: spotId
-//     }
-//   });
-//   return res.json(allSpotBookings);
-// }));
-  
 // ====== post a new booking to the database =======
 router.post('/new', asyncHandler(async (req, res) => {
   const { guest, spot, checkIn, checkOut } = req.body;
@@ -34,5 +23,25 @@ router.post('/new', asyncHandler(async (req, res) => {
   });
   res.status(204).send();
 }));
+
+// ====== post a new booking to the database =======
+router.delete('/', asyncHandler(async (req, res) => {
+  const { bookingId } = req.body;
+  const deadBooking = await Booking.findByPk(bookingId);
+  await deadBooking.destroy();
+  res.status(204).send();
+}));
+
+// // ====== get all the bookings for current spot =======
+// router.get('/:id', asyncHandler(async (req, res) => {
+//   const spotId = parseInt(req.params, 10);
+//   const allSpotBookings = await Booking.findByPk({
+//     where: {
+//       spot: spotId
+//     }
+//   });
+//   return res.json(allSpotBookings);
+// }));
+  
 
 module.exports = router;
